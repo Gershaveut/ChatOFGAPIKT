9a162c35b5d1b68d37cb62c7c4b285e1fe3a2346
Gershaveut <vova1234567890012011@yandex.ru>
2024-05-13 10:49:43 +0300

    The message is now of json type

--- src/com/gershaveut/coapikt/Message.kt
+++ src/com/gershaveut/coapikt/Message.kt
@@ -1,37 +1,21 @@
 package com.gershaveut.coapikt
 
-class Message(var text: String, var messageType: MessageType = MessageType.Message) {
-	var arguments: String? = null
-	
-	override fun equals(other: Any?): Boolean {
-		when (other) {
-			is MessageType -> return messageType == other
-			is String -> return text == other
-		}
-		
-		return super.equals(other)
-	}
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+data class Message(var text: String, var messageType: MessageType = MessageType.Message) {
+	var argument: String? = null
 	
 	override fun toString(): String {
-		return "$messageType:$text"
-	}
-	
-	override fun hashCode(): Int {
-		var result = text.hashCode()
-		result = 31 * result + messageType.hashCode()
-		return result
+		return Gson().toJson(this, type)
 	}
 	
 	companion object {
-		fun createMessageFromText(text: String): Message {
+		val type = object : TypeToken<Message>() {}.type
+		
+		fun parseMessage(text: String): Message {
 			return try {
-				val splitText = text.split(':')
-				val messageType = MessageType.valueOf(splitText[0])
-				
-				Message(if (messageType.haveArguments) splitText[1] else text.substring(text.indexOf(':') + 1), messageType).apply {
-					if (messageType.haveArguments && splitText.count() > 2)
-						arguments = splitText[2]
-				}
+				return Gson().fromJson(text, type)
 			} catch (_: Exception) {
 				Message(text)
 			}
