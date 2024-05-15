6ef958a26ee9f1cecd987af7495bba5d92ae82eb
Gershaveut <vova1234567890012011@yandex.ru>
2024-05-15 10:52:10 +0300

    Added colored text

--- src/com/gershaveut/coapikt/Message.kt
+++ src/com/gershaveut/coapikt/Message.kt
@@ -1,26 +1,38 @@
 package com.gershaveut.coapikt
 
-import com.google.gson.Gson
+import com.google.gson.*
 import com.google.gson.reflect.TypeToken
-import java.awt.Color
+import java.lang.reflect.Type
 
-data class Message(var text: String, var messageType: MessageType = MessageType.Message) {
+data class Message(var text: String, var messageType: MessageType? = MessageType.Message) : JsonDeserializer<Message> {
 	var argument: String? = null
-	var color: Color? = null
+	var color: Array<Int> = arrayOf(0, 0, 0)
 	
 	override fun toString(): String {
-		return Gson().toJson(this, type)
+		return gson.toJson(this, type)
 	}
 	
 	companion object {
-		val type = object : TypeToken<Message>() {}.type
+		val gson = GsonBuilder().registerTypeAdapter(Message::class.java, this).create()
+		val type: Type = object : TypeToken<Message>() {}.type
 		
 		fun parseMessage(text: String): Message {
 			return try {
-				return Gson().fromJson(text, type)
+				return gson.fromJson(text, type)
 			} catch (_: Exception) {
 				Message(text)
 			}
 		}
+	}
+	
+	override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): Message {
+		val jsonObject = json.asJsonObject
+		
+		val text = jsonObject.get("text").asString
+		val messageType = jsonObject.get("messageType")?.asString ?: MessageType.Message.toString()
+		val argument = jsonObject.get("argument")?.asString
+		val color = jsonObject.get("color")?.asJsonArray?.map { it.asInt }?.toTypedArray() ?: arrayOf(0, 0, 0)
+		
+		return Message(text, MessageType.valueOf(messageType)).apply { this.argument = argument; this.color = color }
 	}
 }
